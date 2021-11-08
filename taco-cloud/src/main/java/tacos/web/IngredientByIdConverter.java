b22e69687c776fbee431c7e6d9d36a4cf2beac06
saySthAbout <saysthabout@gmail.com>
2021-11-08 13:19:08 +0900

    3.2.3 JPA 리퍼지터리 선언하기
    JDBC 버전의 리퍼지터리에서는 리퍼지터리가 제공하는 메서드를 우리가 명시적으로 선언하였다. 그러나 스프링 데이터에서는 그 대신 CrudRepository 인터페이스를 확장(extends)할 수 있다.
    
    CrudRepository 인터페이스는 데이터베이스의 CRUD(Create(생성), Read(읽기), Update(변경), Delete(삭제)) 연산을 위한 많은 메서드가 선언되어 있다. CrudRepository는 매개변수화 타입이다. 첫 번째 매개변수는 리퍼지터리에 저장되는 개체 타입이며, 두 번째 매개변수는 개체 ID 속성의 타입이다. IngredientRepository의 경우는 매개변수 타입이 Ingredient와 String이어야 한다.
    
    CrudRepository 인터페이스를 확장한 3개의 인터페이스를 선언했으므로, CrudRepository 인터페이스에 정의된 많은 메서드의 구현을 포함해서 3개의 인터페이스를 구현하는 클래스를 작성해야 한다고 생각할 수 있다. 그러나 그럴 필요가 없다. 이것이 바로 스프링 데이터 JPA의 장점이다! 애플리케이션이 시작될 때 스프링 데이터 JPA가 각 인터페이스 구현체를 자동으로 생성해 주기 때문이다. 이것은 리퍼지터리들이 애당초 사용할 준비가 되어 있다는 것을 의미한다. JDBC 기반의 구현에서 했던 것처럼 그것들을 컨트롤러에 주입만 하면 된다.
    
    부트스트랩 클래스를 변경한 이유는 애플리케이션이 시작되면서 호출되는 dataLoader() 메서드에서 식자재 데이터를 데이터베이스에 미리 저장할 필요가 있기 때문이다(JDBC 기반에서는 애플리케이션이 시작될 때 자동 실행되는 리스트 3.9의 data.sql에서 했다). 만일 이런 요구사항이 없다면 스프링 데이터 JPA를 사용하기 위해 부트스트랩 클래스를 변경할 필요가 없을 것이다.
    
    JDBC 기반에서는 IngredientRepository 인터페이스를 구현하는 리퍼지터리 클래스인 JdbcIngredientRepository의 findById() 메서드가 실행되었다. 그러나 스프링 데이터 JPA에서는 자동으로 구현된 findById() 메서드가 실행되고 데이터베이스에서 식자재를 찾지 못했을 때 null이 반환될 수 있으므로 안전한 처리를 위해 삼항연산자를 사용하여 처리하였다.

--- taco-cloud/src/main/java/tacos/web/IngredientByIdConverter.java
+++ taco-cloud/src/main/java/tacos/web/IngredientByIdConverter.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 
@@ -24,7 +25,8 @@ public class IngredientByIdConverter implements Converter<String, Ingredient>{
 	
 	@Override
 	public Ingredient convert(String id) {
-		return ingredientRepo.findById(id);
+		Optional<Ingredient> optionalIngredient = ingredientRepo.findById(id);
+		return optionalIngredient.isPresent() ? optionalIngredient.get() : null;
 	}
 
 	private void runScripts(List<Resource> resources, boolean continueOnError, String separator, Charset encoding)  {
