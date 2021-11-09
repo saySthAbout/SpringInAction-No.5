1bf7d5b4003d8ca5cb0119844ab36602d5fb9e53
saySthAbout <saysthabout@gmail.com>
2021-11-09 15:04:01 +0900

    4.2.2 JDBC 기반의 사용자 스토어
    passwordEncoder() 메서드는 스프링 시큐리티의 PasswordEncoder 인터페이스를 구현하는 어떤 객체도 인자로 받을 수 있다. 암호화 알고리즘을 구현한 스프링 시큐리티의 모듈에는 다음과 같은 구현 클래스가 포함되어 있다.
    - BCryptPasswordEncoder: bcrypt를 해싱 암호화한다.
    - NoOpPasswordEncoder: 암호화하지 않는다.
    - Pbkdf2PasswordEncoder: PBKDF2를 암호화한다.
    - SCryptPasswordEncoder: scrypt를 해싱 암호화한다.
    - StandardPasswordEncoder: SHA-256을 해싱 암호화한다.
    
    어떤 비밀번호 인코더를 사용하든, 일단 암호화되어 데이터베이스에 저장된 비밀번호는 암호가 해독되지 않는다. 대신 로그인 시에 사용자가 입력한 비밀번호와 동일한 알고리즘을 사용해서 암호화된다. 그다음에 데이터베이스의 암호화된 비밀번호와 비교되며, 이 일은 PasswordEncoder의 matches() 메서드에서 수행되어야 한다.
    
    Execution default-testResources of goal org.apache.maven.plugins:maven-resources- plugin:2.4.3
    에러 잡기 위해서 pom에 artifactId: maven-resources.plugin version:3.2.0 추가함
    
    H2 database schema.sql 안 읽히는 에러 잡기 위해서 application.properties 다시 설정함

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package tacos.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -17,19 +24,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/design", "/orders")
 				.access("hasRole('ROLE_USER')")
 			.antMatchers("/", "/**").access("permitAll")
+			.antMatchers("/h2-console/**").permitAll() // 추가
 		.and()
-			.httpBasic();
+            .csrf() // 추가
+            .ignoringAntMatchers("/h2-console/**").disable() // 추가
+            .httpBasic();
+		/*.and()
+			.httpBasic();*/
 	}
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception{
-		auth.inMemoryAuthentication()
-			.withUser("user1")
+		/*auth.inMemoryAuthentication()
+			.withUser("user1")	
 			.password("{noop}password1")
 			.authorities("ROLE_USER")
 			.and()
 			.withUser("user2")
 			.password("{noop}password2")
 			.authorities("ROLE_USER");
+		*/
+		auth
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from users" +
+										"where username=?")
+				.authoritiesByUsernameQuery("select username, authority from authorities " +
+											"where username=?")
+				.passwordEncoder(new NoEncodingPasswordEncoder());
 	}
+	
+    // Security 무시하기 
+    public void configure(WebSecurity web)throws Exception{
+        web.ignoring().antMatchers("/h2-console/**");
+    }
 }
