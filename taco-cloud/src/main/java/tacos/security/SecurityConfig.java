9c7c085d01a12fc14fea82553c4a4511eae6b1c7
saySthAbout <saysthabout@gmail.com>
2021-11-15 11:01:06 +0900

    4.3.4 CSRF 공격 방어하기
    CSRF(Cross-Site Request Forgery(크로스 사이트 요청 위조))는 많이 알려진 보안 공격이다. 즉, 사용자가 웹사이트에 로그인한 상태에서 악의적인 코드(사이트 간의 요청을 위조하여 공격하는)가 삽입된 페이지를 열면 공격 대상이 되는 웹사이트에 자동으로(그리고 은밀하게) 폼이 제출되고 이 사이트는 위조된 공격 명령이 믿을 수 있는 사용자로부터 제출된 것으로 판단하게 되어 공격에 노출된다.
    
    CSRF 공격을 막기 위해 애플리케이션에서는 폼의 숨김(hidden) 필드에 넣을  CSRF 토큰(token)을 생성할 수 있다. 그리고 해당 필드에 토큰을 넣은 후 나중에 서버에서 사용한다. 이후에 해당 폼이 제출될 때는 폼의 다른 데이터와 함께 토큰도 서버로 전송된다. 그리고 서버에서는 이 토큰을 원래 생성되었던 토큰과 비교하며, 토큰이 일치하면 해당 요청의 처리가 허용된다. 그러나 일치하지 않는다면 해당 폼은 토큰이 있다는 사실을 모르는 악의적인 웹사이트에서 제출된 것이다.
    
    다행스럽게도 스프링 시큐리티에는 내장된 CSRF 방어 기능이 있다. 또한, 이 기능이 기본으로 활성화되어 있어서 우리가 별도로 구성할 필요가 없다. 단지 CSRF 토큰을 넣을 _csrf라는 이름의 필드를 애플리케이션이 제출하는 폼에 포함시키면 된다.
    
    게다가 스프링 시큐리티에서는 CSRF 토큰을 넣는 것조차도 쉽게 해준다. _csrf라는 이름의 요청 속성에 넣으면 되기 때문이다. 이 경우 Thymeleaf 템플릿에서는 다음과 같이 숨김 필드에 CSRF 토큰을 나타낼 수 있다.
    <input type="hidden" name="_csrf" th:value="${_csrf.token}" />
    
    만일 스프링 MVC의 JSP 태그 라이브러리 또는 Thymeleaf를 스프링 시큐리티 dialect와 함께 사용 중이라면 숨김 필드조차도 자동으로 생성되므로 우리가 지정할 필요 없다.
    
    Thymeleaf에서는 <form> 요소의 속성 중 하나가 Thymeleaf 속성임을 나타내는 접두사를 갖도록 하면 된다. 이것은 Thymeleaf가 컨텍스트의 상대 경로를 나타내기 위해 흔히 하는 것이므로 문제가 되지 않는다. 예를 들어, Thymeleaf가 숨김 필드를 포함하도록 하기 위해 다음과 같이 th:action 속성만 지정하면 된다.
    <form method="POST" th:action="@{/login}" id="loginForm">
    CSRF 지원을 비활성화시킬 수도 있다. 그러나 절대로 그렇게 하지 말자. CSRF 방어는 중요하고 폼에서 쉽게 처리되므로 CSRF 지원을 굳이 비활성화할 이유가 없기 때문이다.
    
    다시 말하지만, CSRF 자원을 비활성화하지 말자. 실제 업무용 애플리케이션에서는 특히 그렇다(단, REST API) 서버로 실행되는 애플리케이션의 경우는 CSRF를 disable 해야 한다).

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -17,9 +17,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	DataSource dataSource;
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.antMatchers("/design", "/orders")
+				.access("hasRole('ROLE_USER')")
+			.antMatchers("/", "/**").access("permitAll")
+		
+		.and()
+			.formLogin()
+				.loginPage("/login")
+		.and()
+			.logout()
+				.logoutSuccessUrl("/")
+		
+		.and()
+			.csrf()
+		;
+	}
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -30,59 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests()
-		.antMatchers("/design", "/orders")
-		.access("hasRole('ROLE_USER')")
-		.antMatchers("/", "/**").access("permitAll")
-		.and()
-		.formLogin()
-		.loginPage("/login");
-	}
-	
-	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception{
-		/*auth.inMemoryAuthentication()
-			.withUser("user1")	
-			.password("{noop}password1")
-			.authorities("ROLE_USER")
-			.and()
-			.withUser("user2")
-			.password("{noop}password2")
-			.authorities("ROLE_USER");
-		*/
-		/*auth
-			.jdbcAuthentication()
-				.dataSource(dataSource)
-				.usersByUsernameQuery("select username, password, enabled from users" +
-										"where username=?")
-				.authoritiesByUsernameQuery("select username, authority from authorities " +
-											"where username=?")
-				.passwordEncoder(new NoEncodingPasswordEncoder());
-		*/
-		/*auth
-			.ldapAuthentication()
-			.userSearchBase("ou=people")
-			.userSearchFilter("(uid={0}")
-			.groupSearchBase("ou=groups")
-			.groupSearchFilter("member={0}")
-			.contextSource()
-			.root("dc=tacocloud,dc=com")
-			.ldif("classpath:users.ldif")
-			.and()
-			.passwordCompare()
-			.passwordEncoder(new BCryptPasswordEncoder())
-			.passwordAttribute("userPasscode"); // 여기서는 classpath의 루트에서 users.ldif 파일을 찾아 LDAP 서버로 데이터를 로드하라고 요청한다.
-		*/
 		auth
 			.userDetailsService(userDetailsService)
 			.passwordEncoder(encoder());
 		
 	}
-	
-    // Security 무시하기 
-    public void configure(WebSecurity web)throws Exception{
-        web.ignoring().antMatchers("/h2-console/**");
-    }
 }
