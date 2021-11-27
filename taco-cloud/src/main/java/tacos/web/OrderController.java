340aa1d6e04bcd5168798f8598a597b141721091
saySthAbout <saysthabout@gmail.com>
2021-11-27 17:56:29 +0900

    5.2 우리의 구성 속성 생성하기

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -2,8 +2,12 @@ package tacos.web;
 
 import javax.validation.Valid;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,7 +25,14 @@ import tacos.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
+	
+	private int pageSize = 20;
+	
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
 	
 	private OrderRepository orderRepo;
 	
@@ -64,5 +75,15 @@ public class OrderController {
 		sessionStatus.setComplete();
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping
+	public String ordersForUser(
+		@AuthenticationPrincipal User user, Model model) {
+		
+		Pageable pageable = PageRequest.of(0, pageSize);
+		model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+		
+		return "orderList";
 	}
 }
