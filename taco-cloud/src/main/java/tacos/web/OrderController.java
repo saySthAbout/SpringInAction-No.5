00b84691188e4f0722bc045ef36766ef08fea9c6
saySthAbout <saysthabout@gmail.com>
2021-11-27 18:06:46 +0900

    5.2.1 구성 속성 홀더 정의하기

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -21,23 +21,19 @@ import tacos.Order;
 import tacos.User;
 import tacos.data.OrderRepository;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
-@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
 	
-	private int pageSize = 20;
-	
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
+	private OrderProps props;
 	
 	private OrderRepository orderRepo;
 	
-	public OrderController(OrderRepository orderRepo) {
+	public OrderController(OrderRepository orderRepo,
+		OrderProps props) {
 		this.orderRepo = orderRepo;
+		this.props = props;
 	}
 	
 	@GetMapping("/current")
@@ -81,7 +77,7 @@ public class OrderController {
 	public String ordersForUser(
 		@AuthenticationPrincipal User user, Model model) {
 		
-		Pageable pageable = PageRequest.of(0, pageSize);
+		Pageable pageable = PageRequest.of(0, props.getPageSize());
 		model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
 		
 		return "orderList";
